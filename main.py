a50f942f64f63f2d8154708c01f121b6d0fddd1a
borrisCodes <johnsonbcameron@gmail.com>
2025-05-20 01:58:54 -0400

    Integrated TMDB API for movie information This database allows for more specific keywords and attributes to movies that will make the searching algorithm better

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import os
+from dotenv import load_dotenv
 from maniac import convert_stars_to_numerical
+from tmdbv3api import TMDb, Movie
 from letterboxdpy import user
-from letterboxdpy import movie
 
+load_dotenv()
+
+tmdb = TMDb()
+tmdb_movie_api = Movie()
 letterboxd_username = "Borris"
+
+key_in_env = "API_KEY"
+retrieved_tmdb_api_key = os.environ.get(key_in_env)
+
+if retrieved_tmdb_api_key:
+    tmdb.api_key = retrieved_tmdb_api_key
+    print(f"API key loaded successfully.")
 
 try:
     user_instance = user.User(letterboxd_username)
@@ -12,6 +25,7 @@ try:
     for title, id, slug, rating in films_watched_data:
         films_watched.append({
             "Title":title,
+            'ID':id,
             "Slug":slug,
             "Rating":rating
         })
@@ -19,7 +33,15 @@ try:
     if films_watched:
         for film in films_watched:
             film["NumericalRating"] = convert_stars_to_numerical(film['Rating'])
-            print(film)
+            try:
+                search_results = tmdb_movie_api.search(f"{film['Title']}")
+                if search_results:
+                    tmdb_id = search_results[0].id
+                    details = tmdb_movie_api.details(tmdb_id)
+
+            except Exception as e:
+                print("Error:", e)
+          
     else:
         print(f"Empty list for {letterboxd_username} or an error occured.")
     
